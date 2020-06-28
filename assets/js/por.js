187b3409eda616a8773a2e6679c06d74101e1275
Chacal1993 <ale.gonzalez2@gmail.com>
2020-06-28 22:11:00 +0200

    Porfolio -v Beta

---
+++ assets/js/por.js
@@ -0,0 +1,38 @@
+
+new fullpage('#fullpage', {
+    sectionsColor: ['#e7dfd5', '#84a9ac', '#3b6978', '#204051'],
+});
+
+function animacion() {
+    let classe = document.querySelector('body.fp-viewing-page3');
+    if (classe != null) {
+        move()
+    }
+}
+onmousewheel = animacion
+
+var i = 0;
+function move() {
+    if (i == 0) {
+        i = 1;
+        let progressBar = document.getElementsByClassName("skill_bar_progress");
+
+        for (const iterator of progressBar) {
+            let id = setInterval(load, 20);
+            let width = 1;
+            let value = iterator.getAttribute('aria-valuenow');;
+            console.log(value)
+            function load() {
+                if (width >= value) {
+                    clearInterval(id);
+                    i = 0;
+                } else {
+                    width++;
+                    iterator.style.width = width + "%";
+                }
+            }
+        }
+
+    }
+}
+
